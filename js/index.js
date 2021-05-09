ddbbdc649e92c950d7630b67f919c28f9fa0f5ea
LuisHCR <luishcr.project@protonmail.com>
2021-05-09 22:32:31 +0200

    Añadimos indice blog

--- js/index.js
+++ js/index.js
@@ -3,3 +3,15 @@ function showmenu() {
     document.getElementById("id-links").classList.toggle("nav-links_hider");
 }
 
+/*
+const $detailsList = document.querySelectorAll('details') 
+$detailsList.forEach(($details) => {
+    $details.querySelector('summary').addEventListener('click'), expand)
+    })
+
+function expand() {
+    $detailsList.forEach(($details) => {
+        $details.removeAttribute('open')
+    })
+}
+*/
