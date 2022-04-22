07b16aca34d2d9e036fffaa6eff71a900085a94a
LuisHCR <luishcr.project@protonmail.com>
2022-04-22 13:58:26 +0200

    cambios main.js

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Función para mostar/ocultar menú de navegación responsive
+const navToggle = document.querySelector(".nav-toggle");
+navToggle.addEventListener("click", showMenu);
+
+function showMenu() {
+  document.querySelector("#id-links").classList.toggle("nav-links_hider");
+}
+
+// Zoom en certificados
+const bntImgClose = document.querySelector("#btn-img_close");
+const bntImgForward = document.querySelector("#btn-img_forward");
+const bntImgBackbard = document.querySelector("#btn-img_backward");
+const certificateImgs = document.querySelectorAll(
+  ".main-certificates__item > img"
+);
+const lightbox = document.querySelector(".certificate__principal-container");
+const imgActive = document.querySelector(".main-certificates #img-active");
+
+let imgNumber = 0;
+
+// Abre lightbox
+const openLightbox = (event) => {
+  imgActive.src = event.target.src;
+  lightbox.style.display = "flex";
+  imgNumber = Array.from(certificateImgs).indexOf(event.target);
+};
+
+certificateImgs.forEach((certificate) => {
+  certificate.addEventListener("click", openLightbox);
+});
+
+// Cierra lightbox
+bntImgClose.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+// Avanza lightbox
+const certificateForward = () => {
+  if (imgNumber === certificateImgs.length - 1) {
+    imgNumber = -1;
+  }
+  imgActive.src = certificateImgs[imgNumber + 1].src;
+  imgNumber++;
+};
+bntImgForward.addEventListener("click", certificateForward);
+
+// Retrocede lightbox
+const certificateBackward = () => {
+  if (imgNumber === 0) {
+    imgNumber = certificateImgs.length;
+  }
+
+  imgActive.src = certificateImgs[imgNumber - 1].src;
+  imgNumber--;
+};
+bntImgBackbard.addEventListener("click", certificateBackward);
